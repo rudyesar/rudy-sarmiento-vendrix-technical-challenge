6a11fe29c6c53d0d549c8e3fa3e98888a8bcd230
Rudy Sarmiento <83668987+rudyesar@users.noreply.github.com>
2022-09-21 13:30:32 -0700

    Adjusted schema to include more fields and custom error messages

--- frontend/src/schemas/users.ts
+++ frontend/src/schemas/users.ts
@@ -33,13 +33,22 @@ export const User = z.object({
   _id: z.string().uuid(),
   role: userRoleSchema,
   name: z.object({
-    familyName: z.string().min(10).max(3000),
-    givenName: z.string().min(10).max(2000),
+    familyName: z.string().min(1, { message: "Must be 1 or more characters long" }).max(100, { message: "Must be 100 or fewer characters long" }),
+    givenName: z.string().min(1, { message: "Must be 1 or more characters long" }).max(100, { message: "Must be 100 or fewer characters long" }),
     middleName: z.string().optional(),
     suffix: z.string().optional(),
     title: z.string().optional(),
   }),
-  email: z.string().email(),
+  email: z.string().email({ message: "Invalid email address" }),
+  phone: z.string().min(5, { message: "Must be 5 or more characters long" }).max(20, { message: "Must be 20 or fewer characters long" }),
+  age: z.number().optional(),
+  address: z.object({
+    address1: z.string().min(2, { message: "Must be 2 or more characters long" }).max(100, { message: "Must be 100 or fewer characters long" }),
+    address2: z.string().optional(),
+    city: z.string().min(2, { message: "Must be 2 or more characters long" }).max(100, { message: "Must be 100 or fewer characters long" }),
+    state: z.string().min(2, { message: "Must be 2 or more characters long" }).max(100, { message: "Must be 100 or fewer characters long" }),
+    zipCode: z.number()
+  })
 });
 export type User = z.infer<typeof User>;
 
