bfabe00fb72c15f9ba518f67986c062534369de8
Rudy Sarmiento <83668987+rudyesar@users.noreply.github.com>
2022-09-21 14:14:58 -0700

    Made sure all fields are being validated

--- frontend/src/components/forms/UserCreateUpdateForm.tsx
+++ frontend/src/components/forms/UserCreateUpdateForm.tsx
@@ -170,7 +170,7 @@ export default function UserCreateUpdateForm({ user, onClose }: Props) {
                 />
                 <StringInput
                   fieldName='address.zipCode'
-                  label='ZipCode'
+                  label='Zip Code'
                   control={control}
                 />
               </Stack>
